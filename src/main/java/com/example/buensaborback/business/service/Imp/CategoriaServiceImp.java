860a44458b1c386041e0f0c4b5fd7438fc278fe0
jazminRillo <jazmin.rillo@gmail.com>
2024-05-20 10:39:16 -0300

    Categoria padre funcionando

--- src/main/java/com/example/buensaborback/business/service/Imp/CategoriaServiceImp.java
+++ src/main/java/com/example/buensaborback/business/service/Imp/CategoriaServiceImp.java
@@ -5,17 +5,24 @@ import com.example.buensaborback.business.service.CategoriaService;
 import com.example.buensaborback.business.service.DomicilioService;
 import com.example.buensaborback.domain.entities.Categoria;
 import com.example.buensaborback.domain.entities.Domicilio;
+import com.example.buensaborback.domain.entities.Sucursal;
 import com.example.buensaborback.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Set;
+
 @Service
 public class CategoriaServiceImp extends BaseServiceImp<Categoria,Long> implements CategoriaService {
 
     @Autowired
     CategoriaRepository categoriaRepository;
+
+    @Autowired
+    SucursalServiceImpl sucursalService;
 
     @Override
     public Page<Categoria> findByEsInsumoTrue(Pageable pageable) {
@@ -26,4 +33,36 @@ public class CategoriaServiceImp extends BaseServiceImp<Categoria,Long> implemen
     public Page<Categoria> findByEsInsumoFalse(Pageable pageable) {
         return categoriaRepository.findByEsInsumoFalse(pageable);
     }
+
+    @Override
+    public Categoria create(Categoria categoria) {
+        System.out.println("entre");
+        mapearSubcategorias(categoria);
+        System.out.println(categoria.getSucursales());
+        if (categoria.getSucursales() != null && !categoria.getSucursales().isEmpty()) {
+            Set<Sucursal> sucursales = categoria.getSucursales();
+            System.out.println("buscando sucursales");
+            for (Sucursal sucursal : sucursales) {
+                System.out.println(sucursal);
+                Sucursal sucursalBD = sucursalService.getById(sucursal.getId());
+                if (sucursalBD == null) {
+                    throw new RuntimeException("La sucursal con el id " + sucursal.getId() + " no existe.");
+                }
+                sucursal.getCategorias().add(categoria);
+                sucursalService.update(sucursal, sucursal.getId());
+            }
+        }
+        mapearSubcategorias(categoria);
+        return categoriaRepository.save(categoria);
+    }
+
+    private void mapearSubcategorias(Categoria categoria){
+        if (!categoria.getSubCategorias().isEmpty()){
+            for(Categoria subcategoria: categoria.getSubCategorias()){
+                subcategoria.setCategoriaPadre(categoria);
+                mapearSubcategorias(subcategoria);
+            }
+        }
+    }
+
 }
