13249c69bb943d5364e4b3e3e9d75579329a66c7
AbrilSabatini <abrilsabatini2004@gmail.com>
2024-06-06 09:09:26 -0300

    Traer pedidos por sucursal

--- src/main/java/com/example/buensaborback/business/service/PedidoService.java
+++ src/main/java/com/example/buensaborback/business/service/PedidoService.java
@@ -2,8 +2,13 @@ package com.example.buensaborback.business.service;
 
 import com.example.buensaborback.business.service.Base.BaseService;
 import com.example.buensaborback.domain.dto.PedidoDtos.PedidoEstadoDto;
+import com.example.buensaborback.domain.entities.ArticuloInsumo;
 import com.example.buensaborback.domain.entities.Pedido;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface PedidoService extends BaseService<Pedido,Long> {
     Pedido cambiarEstado(Pedido request, Long id);
+    Page<Pedido> findBySucursal(Long sucursalId, Pageable pageable);
+
 }
--- src/main/java/com/example/buensaborback/presentation/rest/PedidoController.java
+++ src/main/java/com/example/buensaborback/presentation/rest/PedidoController.java
@@ -1,11 +1,14 @@
 package com.example.buensaborback.presentation.rest;
 
 import com.example.buensaborback.business.facade.Imp.PedidoFacadeImp;
+import com.example.buensaborback.domain.dto.ArticuloInsumoDtos.ArticuloInsumoDto;
 import com.example.buensaborback.domain.dto.PedidoDtos.PedidoDto;
 import com.example.buensaborback.domain.dto.PedidoDtos.PedidoEstadoDto;
 import com.example.buensaborback.domain.dto.PedidoDtos.PedidoGetDto;
 import com.example.buensaborback.domain.entities.Pedido;
 import com.example.buensaborback.presentation.rest.Base.BaseControllerImp;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,5 +23,10 @@ public class PedidoController extends BaseControllerImp<Pedido, PedidoDto, Pedid
     @PutMapping("/cambiarEstado/{id}")
     public ResponseEntity<PedidoEstadoDto> cambiarEstado(@RequestBody PedidoEstadoDto entity, @PathVariable Long id){
         return ResponseEntity.ok(facade.cambiarEstado(entity, id));
+    }
+
+    @GetMapping("/porSucursal/{sucursalId}")
+    public ResponseEntity<Page<PedidoGetDto>> findAllBySucursalId(@PathVariable Long sucursalId, Pageable pageable) {
+        return  ResponseEntity.ok(facade.findBySucursal(sucursalId, pageable));
     }
 }
