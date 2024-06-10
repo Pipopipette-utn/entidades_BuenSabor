06577b70d477c9546ce3aab54fe532c74b4bed6b
AbrilSabatini <abrilsabatini2004@gmail.com>
2024-06-09 21:45:51 -0300

    Empleado por email

--- src/main/java/com/example/buensaborback/presentation/rest/EmpleadoController.java
+++ src/main/java/com/example/buensaborback/presentation/rest/EmpleadoController.java
@@ -4,9 +4,9 @@ import com.example.buensaborback.business.facade.Imp.EmpleadoFacadeImp;
 import com.example.buensaborback.domain.dto.EmpleadoDtos.EmpleadoPostDto;
 import com.example.buensaborback.domain.entities.Empleado;
 import com.example.buensaborback.presentation.rest.Base.BaseControllerImp;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/empleados")
@@ -15,4 +15,17 @@ public class EmpleadoController extends BaseControllerImp<Empleado, EmpleadoPost
     public EmpleadoController(EmpleadoFacadeImp facade) {
         super(facade);
     }
+
+    @GetMapping("/filter")
+    public ResponseEntity<?> findByEmail(@RequestParam String email) {
+        try {
+            EmpleadoPostDto empleado = facade.findByEmail(email);
+            return ResponseEntity.ok(empleado);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
 }
