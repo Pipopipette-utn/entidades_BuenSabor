0c90e8dd3508a6d0dd23e476bed11695c7292847
AbrilSabatini <abrilsabatini2004@gmail.com>
2024-06-11 18:58:02 -0300

    Cambiar rutas de redireccionamiento en mp

--- src/main/java/com/example/buensaborback/presentation/rest/MP/PreferenceMp.java
+++ src/main/java/com/example/buensaborback/presentation/rest/MP/PreferenceMp.java
@@ -1,22 +1,15 @@
 package com.example.buensaborback.presentation.rest.MP;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PreferenceMp {
     private String id;
     private int statusCode;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
 }
