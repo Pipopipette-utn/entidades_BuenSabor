11e34a831d46689c7f06939b33480a3a563ca3b9
AbrilSabatini <abrilsabatini2004@gmail.com>
2024-06-12 13:02:19 -0300

    Revert "ArticuloShortDto con denominacion"
    This reverts commit 2fd5c8db7286a259edbb9540c03eb42627735896.

--- src/main/java/com/example/buensaborback/domain/dto/Articulo/ArticuloShortDto.java
+++ src/main/java/com/example/buensaborback/domain/dto/Articulo/ArticuloShortDto.java
@@ -1,15 +1,7 @@
 package com.example.buensaborback.domain.dto.Articulo;
 
 import com.example.buensaborback.domain.dto.BaseDto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ArticuloShortDto extends BaseDto {
     protected String denominacion;
 }
